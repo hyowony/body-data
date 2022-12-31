a9cbe73e36c28d7aed69976d9a1d6208e74591b9
hyowony <118158856+hyowony@users.noreply.github.com>
2022-12-31 14:54:41 +0900

    민석님 폼데이터 활용 코드 추가

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const app = express();
 const port = 3000;
 const testjson = require('./test/test.json')
 
-//json으로 데이터 보내기 연습 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+//json으로 데이터 보내기 연습, test 폴더안에 test.json의 데이터가 서버를 실행시켯을 때 호출되는 것은 확인했으나, url에서 자료를 변경하는 것은 잘 안 되었다.
+
 app.get('/test', (req,res)=> {
   testjson.t=(req,res)=> 
     testjson.t=req.query.t
     res.json(testjson)
   
 })
+
+/// 민석님 폼 데이터 활용 코드 html 파일 버튼을 만들어서 전송했을 때 내가 입력한 데이터가 그대로 적용된 것을 확인하였다. 
+
+app.get("/", (req,res) => {
+  res.send('<form action="/" enctype="multiaprt/form-data" method="post"><input name="id"/><button  type="submit">전송</button></form>')
+})
+
+app.post("/", (req,res) => {
+  const id = req.body.id
+  res.send(id)
+})
+
 
 app.get('/querystring', (req, res) => {
   const id = req.query.id
